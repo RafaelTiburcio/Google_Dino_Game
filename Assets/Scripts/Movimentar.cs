17ab4650e638401dce479e925812f199bcb11e0a
Rafael Tiburcio <contatorafaeltiburcio@gmail.com>
2022-06-23 22:17:02 -0300

    Gerador inimigos

---
+++ Assets/Scripts/Movimentar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentar : MonoBehaviour
+{
+    public Vector2 direcao;
+
+    public float velocidade;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+    }
+}

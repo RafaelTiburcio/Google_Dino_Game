ff6d70aa6a7b5f367fd2f8ef86dcf2cd55a1cd66
Rafael Tiburcio <contatorafaeltiburcio@gmail.com>
2022-06-25 00:15:46 -0300

    Revisão Gerador inimigos

--- Assets/Scripts/GeradorInimigos.cs
+++ Assets/Scripts/GeradorInimigos.cs
@@ -11,39 +11,63 @@ public class GeradorInimigos : MonoBehaviour
     public float birdYMaximo = 1;
     public float delayInicial;
     public float delayEntreEnemies;
+    public float distanciaMinima = 4;
+    public float distanciaMaxima = 8;
     public float birdSpawnPontuacaoMinima = 300;
     public Jogador jogadorScript;
 
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating("GerarEnemie", delayInicial, delayEntreEnemies);
+        //InvokeRepeating("GerarEnemie", delayInicial, delayEntreEnemies); não é mais necessaria, por causa do tempo.
+        StartCoroutine(GerarEnemie());
     }
 
-    private void GerarEnemie()
+
+    private IEnumerator GerarEnemie()
     {
-        var dado = Random.Range(1, 7);
+        yield return new WaitForSeconds(delayInicial);
 
-        if (jogadorScript.pontos >= birdSpawnPontuacaoMinima && dado <=2)
+        GameObject ultimoInimigoGerado = null;
+
+        var distanciaNecessaria = 0f;
+
+        while(true)
         {
-            //gerar bird
-            var posicaoYAleatoria = Random.Range(birdYMinimo, birdYMaximo);
+                    var geracaoObjetoLiberada = ultimoInimigoGerado == null
+                    || Vector3.Distance(transform.position, ultimoInimigoGerado.transform.position)
+                    >= distanciaNecessaria;
 
-            var posicao = new Vector3(
-                transform.position.x,
-                transform.position.y + posicaoYAleatoria,
-                transform.position.z
-            );
-            Instantiate(birdPreFab, posicao, Quaternion.identity);
+                if (geracaoObjetoLiberada)
+                {
+                    var dado = Random.Range(1, 7);
+
+               if (jogadorScript.pontos >= birdSpawnPontuacaoMinima && dado <=2)
+                                    {
+                                        //gerar bird
+                    var posicaoYAleatoria = Random.Range(birdYMinimo, birdYMaximo);
+
+                    var posicao = new Vector3(
+                         transform.position.x,
+                        transform.position.y + posicaoYAleatoria,
+                        transform.position.z
+                        );
+                    ultimoInimigoGerado = Instantiate(birdPreFab, posicao, Quaternion.identity);
+             }
+                else
+               {
+                    //gerar ground enemie
+                    var quantidadeEnemies = groundEnemiesPrefabs.Length;
+                    var indiceAleatorio = Random.Range(0, quantidadeEnemies);
+                    var groundEnemiesPrefab = groundEnemiesPrefabs[indiceAleatorio];
+                    ultimoInimigoGerado = Instantiate(groundEnemiesPrefab, transform.position, Quaternion.identity);
+               }
+
+                    distanciaNecessaria = Random.Range(distanciaMinima, distanciaMaxima);
         }
-        else
-        {
-            //gerar ground enemie
-        var quantidadeEnemies = groundEnemiesPrefabs.Length;
-        var indiceAleatorio = Random.Range(0, quantidadeEnemies);
-        var groundEnemiesPrefab = groundEnemiesPrefabs[indiceAleatorio];
-        Instantiate(groundEnemiesPrefab, transform.position, Quaternion.identity);
 
+                yield return null;
+               // yield return new WaitForSeconds(delayEntreEnemies);
         }
     }
 }
